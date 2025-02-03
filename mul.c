f2519ed92a58e680a9775793abde229f8f50512b
Anushika Tyagi <anushikatyagi1@gmail.com>
2025-02-03 14:12:06 +0530

    added mul.c

---
+++ mul.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    print("Enter the value of a and b :");
+    scanf("%d %d",&a,&b);
+    print("The multiply of a and b is %d\n",a*b);
+    return 0;
+}
