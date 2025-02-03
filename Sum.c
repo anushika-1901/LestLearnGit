c1fea3a81ee2ba591f5e44d2e17832eaf14c2642
Anushika Tyagi <anushikatyagi1@gmail.com>
2025-02-03 13:58:40 +0530

    initial commit

---
+++ Sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    print("Enter the value of a and b :");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    print("The sum of a and b is %d\n",a+b);
+    return 0;
+}
